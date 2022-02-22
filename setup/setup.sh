eb310c52fd6270e186bc3da458bb612b6ad06964
kokrange <orangebattle@hotmail.com>
2022-02-22 10:34:52 +0800

    Update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
-
-set -e
-
-sleep 10 | echo Sleeping
-
-mongo mongodb://mongo-rs0-0:27017 < replicaSet.js
+while true; do
+    echo "rs.status().ok" | mongo mongodb://mongo-rs0-0:27017 --quiet &&
+    echo "rs.status().ok" | mongo mongodb://mongo-rs0-1:27017 --quiet &&
+    echo "rs.status().ok" | mongo mongodb://mongo-rs0-2:27017 --quiet
+    if [[ 0 == $? ]]; then
+        break
+    fi
+    echo 'Wait for all mongo replica ready...'
+    sleep 1
+done
+cn=$(echo "rs.status().codeName" | mongo mongodb://mongo-rs0-0:27017 --quiet)
+if [[ "$cn" == "NotYetInitialized" ]]; then
+    mongo mongodb://mongo-rs0-0:27017 <replicaSet.js
+fi
