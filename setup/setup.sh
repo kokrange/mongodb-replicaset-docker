441dfb17dd53968543b1fb312a0e6bee8003900e
cheng.huang <cheng.huang@sayweee.com>
2022-03-18 00:43:33 +0800

    update

--- setup/setup.sh
+++ setup/setup.sh
@@ -11,5 +11,21 @@ while true; do
 done
 cn=$(echo "rs.status().codeName" | mongo mongodb://mongo-rs0-0:27017 --quiet)
 if [[ "$cn" == "NotYetInitialized" ]]; then
+    echo "==> Initialize ..."
     mongo mongodb://mongo-rs0-0:27017 <replicaSet.js
+else
+    echo "==> Reconfig ..."
+    while true; do
+
+        host0=$(echo "rs.conf().members[0].host" | mongo mongodb://mongo-rs0-0:27017 --quiet)
+        if [[ "${host0}" == *"27010"* ]]; then
+            break
+        fi
+        echo 'Wait for rs.conf() ready...'
+        sleep 1
+    done
+    mongo mongodb://mongo-rs0-0:27017 <replicaSetReconfig.js
 fi
+
+
+
